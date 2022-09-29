d9d9a72b11e5ed395a67ad9b8e6bfcd64f854365
GHOSt <ghostdevbusiness@gmail.com>
2022-09-29 23:33:22 +0100

    feat: demo page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import 'ghostsui';
+</script>
+
+<main>
+    <slot />
+</main>
+
+<style>
+    main {
+        padding: 32px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,42 @@
 <script lang="ts">
-    import { Turnstile } from '$lib';
+    import { Turnstile, type TurnstileTheme } from '$lib';
+
+    let siteKey = '1x00000000000000000000AA';
+    let theme: TurnstileTheme = 'auto';
 </script>
 
-<Turnstile
-    siteKey="0x4AAAAAAAAn8CCb_WenRWb-"
-    on:turnstile-callback={(e) => alert(e.detail.token)}
-    on:turnstile-error={() => alert('error')}
-    on:turnstile-expired={() => alert('expire')} />
+<section class="row">
+    <label>
+        Demo Key type
+
+        <select bind:value={siteKey}>
+            <option value="1x00000000000000000000AA">Always Pass</option>
+            <option value="2x00000000000000000000AB">Always Block</option>
+            <option value="3x00000000000000000000FF">
+                Force interactive challenge
+            </option>
+        </select>
+    </label>
+
+    <label>
+        Theme
+
+        <select bind:value={theme}>
+            <option value="auto">Auto</option>
+            <option value="dark">Dark</option>
+            <option value="light">Light</option>
+        </select>
+    </label>
+</section>
+
+<section>
+    <Turnstile {siteKey} {theme} />
+</section>
+
+<style lang="scss">
+    label {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+    }
+</style>
