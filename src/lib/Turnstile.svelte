c53ba6251d7da12e0e4b3e86c6921b83722ebc85
Braden Wiggins <braden@fractal-hq.com>
2023-09-27 11:01:12 -0400

    [feat] add missing fields / add documentation

--- src/lib/Turnstile.svelte
+++ src/lib/Turnstile.svelte
@@ -1,85 +1,212 @@
+<script lang="ts" context="module">
+    declare global {
+        interface Window {
+            turnstile: TurnstileObject;
+        }
+    }
+</script>
+
 <script lang="ts">
-    import type { Option, TurnstileSize, TurnstileTheme } from './types.d';
-    import type { SupportedLanguages } from 'turnstile-types';
-    import { createEventDispatcher, onMount } from 'svelte';
+    import type { RenderOptions, TurnstileObject, WidgetId } from './types.d';
     import type { Action } from 'svelte/action';
 
-    const dispatch = createEventDispatcher<{
-        'turnstile-callback': { token: string };
-        'turnstile-error': {};
-        'turnstile-expired': {};
-        'turnstile-timeout': {};
-    }>();
+    import { createEventDispatcher, onMount } from 'svelte';
 
-    let loaded = hasTurnstile();
+    const dispatch = createEventDispatcher<
+        {
+            /**
+             * Callback function invoked upon successful challenge completion.
+             * - Data Attribute - `data-callback`
+             * @param token - The token passed upon successful challenge.
+             */
+            callback: { token: string };
+        } & typeof events
+    >();
+
+    let loaded = windowHasTurnstile();
     let mounted = false;
 
-    let widgetId: string;
+    /**
+     * Represents a rendered Turnstile widget.  Used to identify a specific widget when calling
+     * Turnstile methods.
+     */
+    let widgetId: WidgetId;
 
-    export let siteKey: string;
+    /**
+     * Turnstile is Cloudflare’s smart CAPTCHA alternative. It can be embedded into any website
+     * without sending traffic through Cloudflare and works without showing visitors a CAPTCHA.
+     * @see https://developers.cloudflare.com/turnstile
+     */
+    export let turnstile: TurnstileObject | null = null;
+    $: turnstile =
+        windowHasTurnstile() && window.turnstile ? window.turnstile : null;
 
-    export let appearance: Option<'appearance'> = 'always';
-    export let language: SupportedLanguages | 'auto' = 'auto';
-    export let formsField: string = 'cf-turnstile-response';
-    export let execution: Option<'execution'> = 'render';
-    export let action: string | undefined = undefined;
-    export let cData: string | undefined = undefined;
-    export let retryInterval: number | undefined = 8000;
-    export let retry: Option<'retry'> = 'auto';
-    export let theme: TurnstileTheme = 'auto';
-    export let size: TurnstileSize = 'normal';
-    export let forms = true;
+    /**
+     * Every widget has a sitekey. This sitekey is associated with the corresponding
+     * widget configuration and is created upon the widget creation.
+     * - Data Attribute - `data-sitekey`
+     */
+    export let siteKey: RenderOptions['sitekey'];
+
+    /**
+     * Controls when the widget is visible:
+     * - `"always"` - The widget is visible at all times.
+     * - `"execute"` - The widget is visible only after the challenge begins.
+     * - `"interaction-only"` - The widget is visible only when an interaction is required.
+     *
+     * If a widget is visible, its appearance can be controlled via the `appearance` parameter.
+     * - Data Attribute - `data-appearance`
+     * @see
+     * [appearance-modes](https://developers.cloudflare.com/turnstile/get-started/client-side-rendering/#appearance-modes)
+     */
+    export let appearance: RenderOptions['appearance'] = 'always';
+
+    /**
+     * Language to display, either `"auto"` or an ISO 639-1 two-letter language code.
+     * - Data Attribute - `data-language`
+     * @see [language support FAQ](https://developers.cloudflare.com/turnstile/frequently-asked-questions/#what-languages-does-turnstile-support)
+     */
+    export let language: RenderOptions['language'] = 'auto' as const;
+
+    /**
+     * Execution controls when to obtain the token of the widget and can be on `"render"` (default) or on `"execute"`.
+     * - Data Attribute - `data-execution`
+     * @defaultValue "render"
+     * @see [Execution modes](https://developers.cloudflare.com/turnstile/get-started/client-side-rendering/#execution-modes)
+     */
+    export let execution: RenderOptions['execution'] = 'render';
+
+    /**
+     * A customer value that can be used to differentiate widgets under the same
+     * sitekey in analytics and which is returned upon validation. This can only
+     * contain up to 32 alphanumeric characters including `_` and `-`.
+     * - Data Attribute - `data-action`
+     */
+    export let action: RenderOptions['action'] = undefined;
+
+    /**
+     * A customer payload that can be used to attach customer data to the challenge
+     * throughout its issuance and which is returned upon validation. This can only
+     * contain up to 255 alphanumeric characters including `_` and `-`.
+     * - Data Attribute - `data-cdata`
+     */
+    export let cData: RenderOptions['cData'] =
+        undefined as any as RenderOptions['cData'];
+
+    /**
+     * Time between retry attempts in milliseconds. Value must be between `0` and `900000`
+     * (15 minutes). Only applies when `retry` is set to `auto`.
+     * - Data Attribute - `data-retry-interval`
+     * @defaultValue 8000
+     */
+    export let retryInterval: RenderOptions['retry-interval'] = 8000;
+
+    /**
+     * Automatically retry upon failure to obtain a token or never retry.
+     * - Data Attribute - `data-retry`
+     * @defaultValue "auto"
+     */
+    export let retry: RenderOptions['retry'] = 'auto';
+
+    /**
+     * Controls the behavior when the token of a Turnstile widget has expired.
+     * Can be 'auto', 'manual', or 'never'.
+     * - Data Attribute - `data-refresh-expired`
+     * @defaultValue "auto"
+     */
+    export let refreshExpired: RenderOptions['refresh-expired'] = 'auto';
+
+    /**
+     * The widget theme. Can be `"light"`, `"dark"`, or `"auto"`.
+     * - Data Attribute - `data-theme`
+     */
+    export let theme: RenderOptions['theme'] = 'auto';
+
+    /**
+     * The widget size. Can be 'normal' or 'compact'.
+     * - Data Attribute - `data-size`
+     * @defaultValue "normal"
+     */
+    export let size: RenderOptions['size'] = 'normal';
+
+    /**
+     * The tabindex of Turnstile's iframe for accessibility purposes.
+     * - Data Attribute - `data-tabindex`
+     * @defaultValue 0
+     */
     export let tabIndex = 0;
 
-    onMount(() => {
-        mounted = true;
+    /**
+     * Controls if an input element with the response token is created.
+     * - Data Attribute - `data-response-field`
+     * @defaultValue true
+     */
+    export let responseField: RenderOptions['response-field'] = true;
 
-        return () => {
-            mounted = false;
-        };
-    });
+    /**
+     * Name of the input element.
+     * - Data Attribute - `data-response-field-name`
+     * @defaultValue "cf-turnstile-response"
+     */
+    export let responseFieldName: RenderOptions['response-field-name'] =
+        'cf-turnstile-response';
 
-    function hasTurnstile() {
-        if (typeof window == 'undefined') return null;
-        return 'turnstile' in window;
-    }
-
-    function loadCallback() {
-        loaded = true;
-    }
-
-    function error() {
-        dispatch('turnstile-error', {});
-    }
-
-    function expired() {
-        dispatch('turnstile-expired', {});
-    }
-
-    function timeout() {
-        dispatch('turnstile-timeout', {});
-    }
-
-    function callback(token: string) {
-        dispatch('turnstile-callback', { token });
-    }
-
-    export function reset(): void {
+    /**
+     * Resets the widget.
+     * @param widgetId - The ID of the widget.
+     */
+    export const reset: TurnstileObject['reset'] = (): void => {
         window.turnstile.reset(widgetId);
-    }
+    };
 
-    const turnstile: Action = (node) => {
+    const events = {
+        /**
+         * Callback invoked when there is an error (e.g., network error, challenge failed).
+         * - Data Attribute - `data-error-callback`
+         * @see [Client-side errors](https://developers.cloudflare.com/turnstile/reference/client-side-errors)
+         */
+        'error-callback': {},
+        /**
+         * Callback invoked when the token expires and does not reset the widget.
+         * - Data Attribute - `data-expired-callback`
+         */
+        'expired-callback': {},
+        /**
+         * Callback invoked when the challenge expires.
+         * - Data Attribute - `data-timeout-callback`
+         */
+        'timeout-callback': {},
+        /**
+         * Callback invoked before the challenge enters interactive mode.
+         * - Data Attribute - `data-before-interactive-callback`
+         */
+        'before-interactive-callback': {},
+        /**
+         * Callback invoked when the challenge has left interactive mode.
+         * - Data Attribute - `data-after-interactive-callback`
+         */
+        'after-interactive-callback': {},
+        /**
+         * Callback invoked when a given client/browser is not supported.
+         * - Data Attribute - `data-unsupported-callback`
+         */
+        'unsupported-callback': {},
+    } as const;
+
+    const turnstileAction: Action = (node) => {
         const id = window.turnstile.render(node, {
-            'timeout-callback': timeout,
-            'expired-callback': expired,
-            'error-callback': error,
-            callback,
-
             sitekey: siteKey,
-
-            'response-field-name': formsField,
+            callback: (token: string) => dispatch('callback', { token }),
+            ...Object.fromEntries(
+                Object.keys(events).map((event) => [
+                    event,
+                    () => dispatch(event as keyof typeof events, {}),
+                ]),
+            ),
+            'response-field-name': responseFieldName,
+            'refresh-expired': refreshExpired,
             'retry-interval': retryInterval,
-            'response-field': forms,
+            'response-field': responseField,
             tabindex: tabIndex,
             appearance,
             execution,
@@ -89,7 +216,7 @@
             theme,
             cData,
             size,
-        });
+        })!;
 
         widgetId = id;
 
@@ -99,19 +226,37 @@
             },
         };
     };
+
+    function windowHasTurnstile() {
+        if (typeof window == 'undefined') return null;
+        return 'turnstile' in window;
+    }
+
+    function load() {
+        loaded = true;
+    }
+
+    onMount(() => {
+        mounted = true;
+
+        return () => {
+            mounted = false;
+        };
+    });
 </script>
 
 <svelte:head>
     {#if mounted && !loaded}
         <script
+            async
             src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit"
-            on:load={loadCallback}
-            async></script>
+            on:load={load}>
+        </script>
     {/if}
 </svelte:head>
 
 {#if loaded && mounted}
     {#key $$props}
-        <div use:turnstile />
+        <div use:turnstileAction />
     {/key}
 {/if}
--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,10 +1,260 @@
-import type { TurnstileOptions } from 'turnstile-types';
+/**
+ * Represents a rendered Turnstile widget.  Used to identify a specific widget when calling
+ * Turnstile methods.
+ */
+export type WidgetId = string;
 
-// Remove undefined from option
-export type Option<Key extends keyof TurnstileOptions> = Exclude<
-    TurnstileOptions[Key],
-    undefined
->;
+/**
+ * Turnstile is Cloudflare’s smart CAPTCHA alternative. It can be embedded into any website
+ * without sending traffic through Cloudflare and works without showing visitors a CAPTCHA.
+ * @see https://developers.cloudflare.com/turnstile
+ */
+export interface TurnstileObject {
+    /**
+     * If using synchronous loading, will be called once the DOM is ready.
+     * @see [Explicit Rendering](https://developers.cloudflare.com/turnstile/get-started/client-side-rendering/#explicitly-render-the-turnstile-widget)
+     */
+    ready: (cb: () => any) => void;
 
-export type TurnstileTheme = Option<'theme'>;
-export type TurnstileSize = Option<'size'>;
+    /**
+     * @see [Implicit Rendering](https://developers.cloudflare.com/turnstile/get-started/client-side-rendering/#implicitly-render-the-turnstile-widget)
+     */
+    implicitRender: () => void;
+
+    /**
+     * Excecutes the challenge after the render() function has been called, by invoking the
+     * turnstile.execute(container: string | HTMLElement, jsParams?: RenderParameters) function
+     * separately, decoupling the appearance and rendering of a widget from its execution.
+     * @see [Execution Modes](https://developers.cloudflare.com/turnstile/get-started/client-side-rendering/#execution-modes)
+     */
+    execute: () => void;
+
+    /**
+     * Explicitly renders the Turnstile widget.  The `sitekey` parameter is required. The
+     * `callback` parameter, however, is only required if `execution` is set to `execute`.
+     * @param container - HTML container to render the widget.
+     * @param options - Configuration options for rendering. See {@link RenderOptions}.
+     * @returns A `widgetId` string if successful, otherwise undefined.
+     */
+    render(
+        container: HTMLElement | string,
+        options: RenderOptions,
+    ): string | undefined;
+
+    /**
+     * Resets the widget.
+     * @param widgetId - The ID of the widget.
+     */
+    reset(widgetId: WidgetId): void;
+
+    /**
+     * Removes the widget from the page.
+     * @param widgetId - The ID of the widget.
+     */
+    remove(widgetId: WidgetId): void;
+
+    /**
+     * Obtains the widget's response using its widgetId.
+     * @param widgetId - The ID of the widget.
+     */
+    getResponse(widgetId: WidgetId): string;
+
+    /**
+     * Checks if the widget has expired.
+     * @param widgetId - The ID of the widget.
+     */
+    isExpired(widgetId: WidgetId): boolean;
+}
+
+export type TurnstileOptions = RenderOptions;
+
+/**
+ * Interface for Turnstile rendering parameters.
+ */
+export interface RenderOptions {
+    /**
+     * Every widget has a sitekey. This sitekey is associated with the corresponding
+     * widget configuration and is created upon the widget creation.
+     * - Data Attribute - `data-sitekey`
+     */
+    sitekey: string;
+
+    /**
+     * A customer value that can be used to differentiate widgets under the same
+     * sitekey in analytics and which is returned upon validation. This can only
+     * contain up to 32 alphanumeric characters including `_` and `-`.
+     * - Data Attribute - `data-action`
+     */
+    action?: string;
+
+    /**
+     * A customer payload that can be used to attach customer data to the challenge
+     * throughout its issuance and which is returned upon validation. This can only
+     * contain up to 255 alphanumeric characters including `_` and `-`.
+     * - Data Attribute - `data-cdata`
+     */
+    cData?: string;
+
+    /**
+     * Callback function invoked upon successful challenge completion.
+     * - Data Attribute - `data-callback`
+     * @param token - The token passed upon successful challenge.
+     */
+    callback: (token: string) => void;
+
+    /**
+     * Callback invoked when there is an error (e.g., network error, challenge failed).
+     * - Data Attribute - `data-error-callback`
+     * @see [Client-side errors](https://developers.cloudflare.com/turnstile/reference/client-side-errors)
+     */
+    'error-callback'?: () => void;
+
+    /**
+     * Execution controls when to obtain the token of the widget and can be on `"render"` (default) or on `"execute"`.
+     * - Data Attribute - `data-execution`
+     * @defaultValue "render"
+     * @see [Execution modes](https://developers.cloudflare.com/turnstile/get-started/client-side-rendering/#execution-modes)
+     */
+    execution?: 'render' | 'execute';
+
+    /**
+     * Callback invoked when the token expires and does not reset the widget.
+     * - Data Attribute - `data-expired-callback`
+     */
+    'expired-callback'?: () => void;
+
+    /**
+     * Callback invoked before the challenge enters interactive mode.
+     * - Data Attribute - `data-before-interactive-callback`
+     */
+    'before-interactive-callback'?: () => void;
+
+    /**
+     * Callback invoked when the challenge has left interactive mode.
+     * - Data Attribute - `data-after-interactive-callback`
+     */
+    'after-interactive-callback'?: () => void;
+
+    /**
+     * Callback invoked when a given client/browser is not supported.
+     * - Data Attribute - `data-unsupported-callback`
+     */
+    'unsupported-callback'?: () => void;
+
+    /**
+     * The widget theme. Can be `"light"`, `"dark"`, or `"auto"`.
+     * - Data Attribute - `data-theme`
+     */
+    theme?: 'light' | 'dark' | 'auto';
+
+    /**
+     * Language to display, either `"auto"` or an ISO 639-1 two-letter language code.
+     * - Data Attribute - `data-language`
+     * @see [language support FAQ](https://developers.cloudflare.com/turnstile/frequently-asked-questions/#what-languages-does-turnstile-support)
+     */
+    language?: SupportedLanguages | 'auto' | ISO;
+
+    /**
+     * The tabindex of Turnstile's iframe for accessibility purposes.
+     * - Data Attribute - `data-tabindex`
+     * @defaultValue 0
+     */
+    tabindex?: number;
+
+    /**
+     * Callback invoked when the challenge expires.
+     * - Data Attribute - `data-timeout-callback`
+     */
+    'timeout-callback'?: () => void;
+
+    /**
+     * Controls if an input element with the response token is created.
+     * - Data Attribute - `data-response-field`
+     * @defaultValue true
+     */
+    'response-field'?: boolean;
+
+    /**
+     * Name of the input element.
+     * - Data Attribute - `data-response-field-name`
+     * @defaultValue "cf-turnstile-response"
+     */
+    'response-field-name'?: string;
+
+    /**
+     * The widget size. Can be 'normal' or 'compact'.
+     * - Data Attribute - `data-size`
+     * @defaultValue "normal"
+     */
+    size?: 'normal' | 'compact';
+
+    /**
+     * Automatically retry upon failure to obtain a token or never retry.
+     * - Data Attribute - `data-retry`
+     * @defaultValue "auto"
+     */
+    retry?: 'auto' | 'never';
+
+    /**
+     * Time between retry attempts in milliseconds. Value must be between `0` and `900000`
+     * (15 minutes). Only applies when `retry` is set to `auto`.
+     * - Data Attribute - `data-retry-interval`
+     * @defaultValue 8000
+     */
+    'retry-interval'?: number;
+
+    /**
+     * Controls the behavior when the token of a Turnstile widget has expired.
+     * Can be 'auto', 'manual', or 'never'.
+     * - Data Attribute - `data-refresh-expired`
+     * @defaultValue "auto"
+     */
+    'refresh-expired'?: 'auto' | 'manual' | 'never';
+
+    /**
+     * Controls when the widget is visible:
+     * - `"always"` - The widget is visible at all times.
+     * - `"execute"` - The widget is visible only after the challenge begins.
+     * - `"interaction-only"` - The widget is visible only when an interaction is required.
+     *
+     * If a widget is visible, its appearance can be controlled via the `appearance` parameter.
+     * - Data Attribute - `data-appearance`
+     * @see
+     * [appearance-modes](https://developers.cloudflare.com/turnstile/get-started/client-side-rendering/#appearance-modes)
+     */
+    appearance?: 'always' | 'execute' | 'interaction-only';
+
+    chlPageData?: string; // ENTERPRISE ONLY - undocumented
+}
+
+export type ISO = string & {};
+
+/**
+ * A list of supported languages for Turnstile.
+ * @see
+ * [language support FAQ](https://developers.cloudflare.com/turnstile/frequently-asked-questions/#what-languages-does-turnstile-support)
+ */
+export type SupportedLanguages =
+    | 'ar-eg'
+    | 'ar'
+    | 'de'
+    | 'en'
+    | 'es'
+    | 'fa'
+    | 'fr'
+    | 'id'
+    | 'it'
+    | 'ja'
+    | 'ko'
+    | 'nl'
+    | 'pl'
+    | 'pt'
+    | 'pt-br'
+    | 'ru'
+    | 'tlh'
+    | 'tr'
+    | 'uk'
+    | 'uk-ua'
+    | 'zh'
+    | 'zh-cn'
+    | 'zh-tw';
